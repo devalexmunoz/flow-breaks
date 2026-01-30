c5b88e8a3970e2ee6197e23f74291feabe82a388
Alex Munoz <alex@yurnylabs.com>
2026-01-30 10:57:36 -0700

    build(frontend): Setup Vue 3 and Inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import { Head } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <Head title="Home" />
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <h1 class="text-4xl font-bold text-gray-800">Home</h1>
+  </div>
+</template>
